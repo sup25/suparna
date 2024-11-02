c83c05dc19c315f6e837f5e2d5a91b00a3273b2d
sup25 <suparnaa51@gmail.com>
2024-11-02 11:12:44 +0545

    feature/og-image

--- app/metadata.ts
+++ app/metadata.ts
@@ -13,23 +13,7 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      {
-        url: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-    apple: [{ url: "/apple-touch-icon.png" }],
-  },
+
   openGraph: {
     title: "Suparna Adhikari | Full-Stack Developer & Software Engineer",
     description:
@@ -38,5 +22,13 @@ export const metadata: Metadata = {
     siteName: "Suparna Adhikari",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "/suparna.png",
+        width: 1200,
+        height: 630,
+        alt: "Suparna Adhikari's Profile Preview Image",
+      },
+    ],
   },
 };
